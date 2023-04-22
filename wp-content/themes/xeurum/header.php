fe2e2cf27adf7b95ab0a4ff7806990148b7e9bbd
Vlad Pryma <v.pryma@p1k.org>
2023-04-23 00:49:26 +0300

    added favicon on site

--- wp-content/themes/xeurum/header.php
+++ wp-content/themes/xeurum/header.php
@@ -6,6 +6,11 @@
 	    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link href="https://fonts.googleapis.com/css2?family=Mulish:wght@400;500;600;700;800;900;1000&display=swap" rel="stylesheet">
+        <!-- favicons -->
+        <link rel="icon" href="/wp-content/themes/xeurum/image/favicon/favicon.ico" type="image/x-icon" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/xeurum/image/favicon/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/xeurum/image/favicon/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/xeurum/image/favicon/favicon-16x16.png">
         <?php wp_head(); ?>
 	</head>
 
